214d795002048121852a121566fc8c2cdb102998
johns-coderoom <johnnsenanu36@gmail.com>
2026-01-08 20:17:15 +0100

    Add files via upload

---
+++ test_environment.py
@@ -0,0 +1,10 @@
+import sys
+import scanpy as sc
+import numpy as np
+
+print(f"Python version: {sys.version}")
+print(f"Scanpy version: {sc.__version__}")
+print(f"Numpy version: {np.__version__}")
+
+print("\nEnvironment setup successful!")
+
